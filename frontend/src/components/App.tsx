0e9957defe8d3c9aeb9da486abdd891189110810
Artem Mikhailovsky <artem.mikhailovskyi@gmail.com>
2020-01-27 08:55:46 +0300

    init

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {ProjectWorkspacePage} from "./ServiceDiagram/ProjectWorkspacePage";
+
+const App: React.FC = () => {
+  return (
+      <ProjectWorkspacePage/>
+  );
+};
+
+export default App;
